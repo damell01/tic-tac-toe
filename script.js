3610e7be950c475c0e60fc38b808b3bfa28339de
Damell <damellbell01@gmail.com>
2022-05-01 17:20:50 -0500

    creating elements  for game

---
+++ script.js
@@ -0,0 +1,12 @@
+//elements being manipulated on screen
+const boxes= document.querySelector('.box',) ;
+const text= document.querySelector('#heading') ;
+// selection for which box user chose
+const strategy= document.querySelector('#strategy') ;
+const restartBtn= documentt.querySelector('#restart') ;
+
+const  drawboard  =() => {
+
+
+    
+}
