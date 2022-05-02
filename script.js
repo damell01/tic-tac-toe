025320e31d067cecd834d810a495af523780b4f0
Damell <damellbell01@gmail.com>
2022-05-02 07:59:06 -0500

    logic  for winner

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const text= document.querySelector('#heading') ;
 // selection for which box user chose
 const strategy= document.querySelector('#strategy') ;
 const restartBtn= documentt.querySelector('#restart') ;
-
+const spaces=[];
+//user-choices
+const tick_circle="O" ;
+tick_x='X' ;
+//creates board  
     const drawBoard = () => {
         boxes.forEach((box, i) => {
           let styleString = '';
@@ -27,6 +31,65 @@ const restartBtn= documentt.querySelector('#restart') ;
           box.style = styleString;
           box.addEventListener('click', boxClicked);
         });
+        const boxClicked = (e) => {
+            const id = e.target.id;
+            if (!spaces[id]) {
+              spaces[id] = currentPlayer;
+              e.target.innerText = currentPlayer;
+          
+              if (playerWon()) {
+                text.innerText = `${currentPlayer} has won!`;
+                restart();
+                return;
+              }
+          
+              if (playerDraw()) {
+                return;
+              }
+              currentPlayer = currentPlayer === tick_circle ? tick_x : tick_circle;
+            }
+          };
+          // logic for winner
+          const playerWon = () => {
+            if (spaces[0] === currentPlayer) {
+              if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+                strategy.innerText = `${currentPlayer} wins up to top`;
+                return true;
+              }
+              if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+                strategy.innerText = `${currentPlayer} wins on the left`;
+                return true;
+              }
+              if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+                strategy.innerText = `${currentPlayer} wins diagonally`;
+                return true;
+              }
+            }
+            if (spaces[8] === currentPlayer) {
+              if (spaces[2] === currentPlayer && spaces[5] === currentPlayer) {
+                strategy.innerText = `${currentPlayer} wins on the right`;
+                return true;
+              }
+              if (spaces[6] === currentPlayer && spaces[7] === currentPlayer) {
+                strategy.innerText = `${currentPlayer} wins on the bottom`;
+                return true;
+              }
+            }
+            if (spaces[4] === currentPlayer) {
+              if (spaces[1] === currentPlayer && spaces[7] === currentPlayer) {
+                strategy.innerText = `${currentPlayer} wins vertically on middle`;
+                return true;
+              }
+              if (spaces[3] === currentPlayer && spaces[5] === currentPlayer) {
+                strategy.innerText = `${currentPlayer} wins horizontally on the middle`;
+                return true;
+              }
+              if (spaces[2] === currentPlayer && spaces[6] === currentPlayer) {
+                strategy.innerText = `${currentPlayer} wins diagonally`;
+                return true;
+              }
+            }
+          };
       };
 
 
